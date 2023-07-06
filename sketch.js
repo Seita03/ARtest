62fc40168ab9e71fe4363cfaea7ab18f96fbed49
Seita03 <seitak130301@gmail.com>
2023-07-06 16:26:30 +0900

    Third commit

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function getStream(video){
     return navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
-            facingMode: "environment"
+            facingMode: "user"
         }
     })
     .then((stream)=>{
